88cdc94e71f308099a1887305ade373be160311f
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-11-13 15:56:23 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -260,15 +260,15 @@ cat > templates/show_number.html <<EOL
 </html>
 EOL
 
+# Deactivate the virtual environment
+deactivate
+
 cat > init_db.py <<EOL
 from app import db
 db.create_all()
 EOL
 
 python3 init_db.py
-
-# Deactivate the virtual environment
-deactivate
 
 if [ "$DOMAIN" != "localhost" ]; then
     whiptail --title "DNS Configuration" --msgbox "Ensure your domain name's DNS settings are correctly configured:\n\n1. Set an A record that points your domain to your server's public IP address.\n2. Wait for the DNS changes to propagate, which might take some time.\n\nAfter confirming these settings, the script will attempt to acquire an SSL certificate for your domain." 15 60
